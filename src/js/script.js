3cf6a9e6573402157fbac07decbb3b1beb66a16f
Daniel Silva <118705247+Daniiell99e@users.noreply.github.com>
2024-10-19 22:29:29 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -52,4 +52,45 @@ prevBtn.addEventListener('click', () => {
             scrollPosition = Math.max(scrollPosition - itemWidth, 0);
             carousel.style.transform = `translateX(-${scrollPosition}px)`;
         });
+//carrocel - final
 
+//slide inicio
+        let currentIndex = 0;
+        const slides = document.querySelectorAll('.slide');
+        const totalSlides = slides.length;
+                
+        function showSlide(index) {
+        // Remove qualquer classe zoom ou reset de todas as imagens
+        slides.forEach(slide => slide.classList.remove('zoom', 'reset'));
+                
+        // Calcula o índice da imagem atual
+        currentIndex = (index + totalSlides) % totalSlides;
+                
+        // Exibe a imagem atual (sem ainda aplicar o zoom)
+        document.querySelector('.slides').style.transform = `translateX(${-currentIndex * 100}%)`;
+                
+        // Após exibir, aplica o zoom in na imagem atual
+        setTimeout(() => {
+        slides[currentIndex].classList.add('zoom');
+        }, 1000); // Dá meio segundo para a transição de slide antes de aplicar o zoom
+                
+        // Depois de 2 segundos, remove o zoom (zoom out)
+            setTimeout(() => {
+                slides[currentIndex].classList.remove('zoom');
+                slides[currentIndex].classList.add('reset');
+                }, 2700); // 2 segundos para o efeito de zoom in, depois zoom out
+            }
+                
+        // Função para iniciar o slider automaticamente
+        function startSlider() {
+            showSlide(currentIndex); // Exibe a primeira imagem
+                
+                setInterval(() => {
+                    currentIndex++; // Incrementa para a próxima imagem
+                    showSlide(currentIndex); // Exibe a próxima imagem e aplica o zoom
+                }, 6000); // 5 segundos para cada ciclo (tempo para zoom in, zoom out e troca de imagem)
+        }
+                
+        // Inicia o slider
+        startSlider();
+    // slide - final
