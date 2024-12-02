c59b442670c45e5ff1778165eaa66459cca3661c
Daniel Silva <118705247+Daniiell99e@users.noreply.github.com>
2024-12-01 22:28:46 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -206,7 +206,7 @@ function selectCity(city, event) {
     
     try {
       localStorage.setItem('dadosViagem', JSON.stringify(destination));
-      window.location.href = '../pages/seusRoteiros.html';
+      window.location.href = 'https://daniiell99e.github.io/Projeto_integrador/src/pages/SeusRoteiros.html';
     } catch (error) {
       console.error('Erro ao salvar dados:', error);
       alert('Ocorreu um erro ao salvar os dados. Por favor, tente novamente.');
