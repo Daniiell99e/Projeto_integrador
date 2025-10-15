4e6898b69faebc2ce4f0d5efccb1ffc5c013f99b
Fabricio <granjeirodecarvalhof@gmail.com>
2025-10-15 00:06:44 -0300

    Removendo arquivos antigos

--- login/mostrarSenha.js
+++
@@ -1,7 +0,0 @@
-  const togglePassword = document.querySelector(".toggle-password");
-  const passwordInput = document.getElementById("senha");
-
-  togglePassword.addEventListener("click", () => {
-    const isPassword = passwordInput.type === "password";
-    passwordInput.type = isPassword ? "text" : "password";
-  });
